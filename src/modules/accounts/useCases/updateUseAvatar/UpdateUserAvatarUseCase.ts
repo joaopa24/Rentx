5b3ee4e223f7595ec8084f9ef3c6a613c1b99c27
joknz21 <joaokn129@gmail.com>
2022-04-16 14:50:37 -0300

    utils

--- src/modules/accounts/useCases/updateUseAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUseAvatar/UpdateUserAvatarUseCase.ts
@@ -6,6 +6,8 @@
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
+import { deleteFile } from "../../../../utils/file"
+
       // criar controller
 interface IRequest {
     user_id: string;
@@ -20,7 +22,10 @@ class UpdateUserAvatarUseCase {
     ) {}
     async execute({ user_id, avatar_file}: IRequest): Promise<void> {
        const user = await this.usersRepository.findById(user_id)
-
+       
+       if(user.avatar){
+        await deleteFile(`./tmp/avatar/${user.avatar}`)
+       }
        user.avatar = avatar_file
        await this.usersRepository.create(user)
     }
